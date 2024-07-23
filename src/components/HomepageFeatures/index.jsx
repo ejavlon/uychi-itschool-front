ef9c0a8801bb2bf69723805f5e73437e3b7dae50
Javlon Ergashev <javlon2677572@gmail.com>
2024-07-23 19:20:40 +0500

    updated

--- src/components/HomepageFeatures/index.jsx
+++ src/components/HomepageFeatures/index.jsx
@@ -1,20 +1,50 @@
-import React, { useEffect } from 'react'
+// import React, { useEffect } from 'react'
+// import Particle from './ParticleFeatures/Particle';
+
+// export default function HomepageFeatures() {
+//   useEffect(() => {
+//     const loadReplainJS = () => {      
+//       const script = document.createElement('script');      
+//       script.src = './replain.js';
+//       script.onload = () => initializeParticlesJS();
+//       document.body.appendChild(script);
+//     };    
+
+//     loadReplainJS();
+//   },[])
+//   return (
+//     <React.Fragment>
+//       <Particle/>
+//     </React.Fragment>
+//   )
+// }
+import React, { useEffect } from 'react';
 import Particle from './ParticleFeatures/Particle';
 
 export default function HomepageFeatures() {
   useEffect(() => {
-    const loadReplainJS = () => {      
-      const script = document.createElement('script');      
+    const loadReplainJS = () => {
+      const script = document.createElement('script');
       script.src = './replain.js';
-      script.onload = () => initializeParticlesJS();
+      script.onload = () => {
+        console.log('replain.js loaded');
+        initializeParticlesJS();
+      };
       document.body.appendChild(script);
     };
 
     loadReplainJS();
-  },[])
+
+    // Cleanup script when component unmounts
+    return () => {
+      const scripts = document.querySelectorAll('script[src="./replain.js"]');
+      scripts.forEach(script => script.remove());
+    };
+  }, []);
+
   return (
     <React.Fragment>
-      <Particle/>
+      <Particle />
     </React.Fragment>
-  )
+  );
 }
