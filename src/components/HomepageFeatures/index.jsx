8f8baccbd8f3f05251a696e2ffa690570e4fdba4
Javlon Ergashev <javlon2677572@gmail.com>
2024-07-23 19:37:07 +0500

    updated

--- src/components/HomepageFeatures/index.jsx
+++ src/components/HomepageFeatures/index.jsx
@@ -1,23 +1,3 @@
-// import React, { useEffect } from 'react'
-// import Particle from './ParticleFeatures/Particle';
-
-// export default function HomepageFeatures() {
-//   useEffect(() => {
-//     const loadReplainJS = () => {      
-//       const script = document.createElement('script');      
-//       script.src = './replain.js';
-//       script.onload = () => initializeParticlesJS();
-//       document.body.appendChild(script);
-//     };    
-
-//     loadReplainJS();
-//   },[])
-//   return (
-//     <React.Fragment>
-//       <Particle/>
-//     </React.Fragment>
-//   )
-// }
 import React, { useEffect } from 'react';
 import Particle from './ParticleFeatures/Particle';
 
